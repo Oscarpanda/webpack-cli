b29326fadb2f2d27bb9ee2a73602abf1e8ebb27a
吴高雄 <you@example.com>
2018-06-11 09:59:24 +0800

    ccc

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import Slot from "./slot";
+import xmlReader from "./xmlReader";
+import $ from 'jquery';
+class PXMap {
+    constructor() {
+        this.PXMap = [];
+        this.slot = new Slot();
+        console.log("3")
+        console.log(this.slot.id);
+    }
+    add(id, next) {
+        let slot = new Slot(id, next);
+        this.PXMap.push(slot);
+    }
+    insert(slot,after) {// 添加节点
+        if (after) {
+            this.PXMap.forEach((slot) => {
+                if (slot.id === after) {
+                    let beforeSlot = this.find(after)
+                    console.log(beforeSlot)
+                    beforeSlot.next = slot.id;
+                }
+            })
+        }
+        this.PXMap.push(slot);
+    }
+    show() {
+        console.log(this.PXMap)
+    }
+    find(id){
+        let finded; 
+        this.PXMap.some((value) => {
+            if (value.id === id) {
+                finded = value
+                return true;
+            }
+        })
+        return finded;
+    }
+}
+
+let pxmap = new PXMap()
+let Slot1 = new Slot("1");
+let Slot2 = new Slot("2");
+pxmap.insert(Slot1);
+pxmap.insert(Slot2, "1");
+console.log(pxmap)
+let xmlreader = new xmlReader();
+xmlreader.readVideo()
+xmlreader.readComponent()
+setTimeout(() =>{
+    console.log(xmlreader.ComponentArry, xmlreader.VideoArray)
+    xmlreader.VideoArray.forEach((value)=>{
+        pxmap.add(value.id,value.next); 
+    })
+    pxmap.show();
+
+}, 1000)
