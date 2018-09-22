e8d59f59bd1697ca52163dbc8bf0ec723a304c23
Oscarpanda <wu_gaoxiong@163.com>
2018-09-22 19:24:20 +0800

    111

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import echarts from 'echarts';
 import Rx from 'rxjs/Rx';
 import $ from 'jquery';
+import { funMap } from './funmap';
+console.log(funMap);
 Rx.Observable.of(1,2,3)
 let arr = numberCreator();
 var myChart = echarts.init(document.getElementById('main'));
@@ -36,33 +38,9 @@ function numberCreator() {
 // 选择排序
 
 
-let arrquick = numberCreator();
-console.log(quickFun_right(arrquick, 0, arrquick.length - 1));
-function select(x, observer) {
-    let arr = x[0];
-    let timer = x[1].timer;
-    for (let i = 0 ; i < arr.length; i++ ){
-        let index = i;
-        let min = arr[index]
-        for (let j = i; j< arr.length;j++){
-            if ( min > arr[j]){
-                min = arr[j];
-                index = j;
-            }
-            
-        }
-        let temp = arr[i];
-        arr[i] = min;
-        arr[index] = temp;
-        let temparr = arr.slice(0, arr.length-1);
-        let obj = {};
-        obj.temparr = temparr;
-        timer++;
-        obj.timer = timer
+// let arrquick = numberCreator();
+// console.log(quickFun_right(arrquick, 0, arrquick.length - 1));
 
-        observer.next(obj);
-    }
-}
 function quickFun (arr) {
     if (arr.length <= 1) {
         return arr;
@@ -100,13 +78,16 @@ function getposition(arr,f,e) {
     swap(arr, i+1, e);
     return i+1;
 }
-function quickFun_right(arr,f,e) {
+function quickFun_right(x,f,e, observer) {
+    let arr = x[0]
     if (e <= f) return;
     // if (f < e) {
-        let q = getposition(arr, f, e);
-        quickFun_right(arr, f, q-1);
-        quickFun_right(arr, q+1, e);
-        return arr;
+    let q = getposition(arr, f, e);
+    quickFun_right(x, f, q-1, observer);
+    quickFun_right(x, q+1, e, observer);
+    x[1]++;
+    observer.next(x)
+    return x;
     // }
  
 }
@@ -146,11 +127,29 @@ const createNumber$ = Rx.Observable.fromEvent($('#numberCreater'), 'click')
 
        // console.log(nums);
     });
-createNumber$.subscribe(x => {
-    console.log('click');
-    console.log(x)
-})
+// createNumber$.subscribe(x => {
+//     console.log('click');
+//     console.log(x)
+    
+// })
 let currentType
+Rx.Observable.prototype.sort = function () {
+    const input = this;
+    return Rx.Observable.create((observer) => {
+        input.subscribe((arr)=> {
+            const num = arr[0];
+            const select = arr[1];
+            const sortFun = funMap[select.type];
+            sortFun(num, (arr) => {
+                observer.next({
+                    nums: JSON.parse(JSON.stringify(arr)),
+                    select: select
+                }
+                )
+            })
+        })
+    })
+}
 const select$ = Rx.Observable.fromEvent($('.sortTypes'), 'change')
     .map(e => e.target)
     .map(x => x.options[x.selectedIndex].value)
@@ -164,22 +163,31 @@ const select$ = Rx.Observable.fromEvent($('.sortTypes'), 'change')
         currentType = x.type
     });
 const combine$ = Rx.Observable.combineLatest(createNumber$, select$)
-var te1 = combine$
+    .sort()
+// var te1 = combine$
     .flatMap(x=>{
-        return Rx.Observable.create((observer) => {
-            select(x, observer);
-        });
-    });
-te1.subscribe(x => {
-        console.log("CONBIME")
-        console.log("x", x);
-        setTimeout(()=>{
-            option.series[0].data = x.temparr;
-            myChart.setOption(option);
+        console.log(x.nums)
+        return Rx.Observable.of(x).delay(100 * x.select.timer++);
+        // return Rx.Observable.create((observer) => {
+        
+            // select(x, observer);
+        // });
+    })
+    .do((x) => {
+        option.series[0].data = x.nums;
+        myChart.setOption(option);
+    })
+    .subscribe();
+// te1.subscribe(x => {
+//         console.log("CONBIME")
+//         console.log("x", x);
+//         setTimeout(()=>{
+//             option.series[0].data = x.temparr;
+//             myChart.setOption(option);
 
-        }, x.timer * 50)
+//         }, x.timer * 50)
 
-    });
+//     });
 select$.subscribe(x => {
     console.log(x)
 })
