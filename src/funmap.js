adcdd3d13db9ea349aaaab5bdd969c324269c1e9
gaoxiong.wu <wugaoxiong@100tal.com>
2018-10-06 16:48:42 +0800

    添加冒泡排序

--- src/funmap.js
+++ src/funmap.js
@@ -1,9 +1,11 @@
 import selectsort from "./select-sort";
 import quicksort from "./quick-sort";
+import upsort from "./up-sort";
 export const funMap = {
     '0': () => {
         console.log("0");
     },
     '1': selectsort,
     '2': quicksort,
+    '3': upsort,
 }
---
+++ src/up-sort.js
@@ -0,0 +1,19 @@
+function upSort(arr, callback) {
+    for (let i = 0; i < arr.length; i++){
+        for(let j = 0 ;j < arr.length - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap (arr, j, j+1);
+            }
+        }
+        callback(arr);
+    }
+    return arr;
+}
+
+function swap (arr, i ,j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
+}
+export default upSort;
